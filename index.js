0f14b5a1de47a2bad43a1d278fbacbc7fed6e73b
gaikwadshrutii <gaikwadshrutii01@gmail.com>
2024-08-26 13:19:26 +0530

    initial setup

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+require("dotenv").config()
+
+const app = express()
+
+// step 1 middelwere
+app.use(express.json())
+app.use(cors({ origin: true, credentials: true }))
+// step 2 route
+app.use("/api/notes", require("./routes/todo.route"))
+// step 3 404
+app.use("*", (req, res) => {
+    res.status(404).json({ message: "Resource Not Found 404" })
+})
+// step 4 error handler
+app.use("*", (err, req, res, next) => {
+    console.log(err)
+    res.status(500).json({ message: "SERVER ERROR", error: err.message })
+})
+// step 5 connection
+mongoose.connect(process.env.MONGO_URL)
+mongoose.connection.once("open", () => {
+    console.log("MONGO CONNECTED")
+    app.listen(process.env.PORT, console.log("SERVER RUNNING")
+    )
+
+})
